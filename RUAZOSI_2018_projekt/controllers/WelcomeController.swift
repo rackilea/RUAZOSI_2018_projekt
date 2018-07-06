58db91a1e330d51411be19a85fd8d1391a423ecb
rackilea <lea.racki@fer.hr>
2018-07-06 22:10:29 +0200

    Initial commit

---
+++ RUAZOSI_2018_projekt/controllers/WelcomeController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeController.swift
+//  RUAZOSI_2018_projekt
+//
+//  Created by Lea Rački on 06/07/2018.
+//  Copyright © 2018 Lea Rački. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
